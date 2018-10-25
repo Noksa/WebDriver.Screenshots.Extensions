dd5a4224b75e366e92cefc44659846e4ec47d6dd
Александр Данилин <a.danilin@iml.ru>
2018-10-25 13:38:36 +0300

    Restore js script

--- WDSE/Scripts/GetElementWithActiveScrollbar.js
+++ WDSE/Scripts/GetElementWithActiveScrollbar.js
@@ -7,17 +7,23 @@
         return $(this)[0].tagName.toLowerCase() !== "html";
     });
     if (elementWithScrollBar.length === 1) return elementWithScrollBar[0];
+    let str = "";
+    elementWithScrollBar.each(function() {
+        str = str + $(this)[0].tagName + ", ";
+    });
+    if (str !== "") {
+        const index = str.lastIndexOf(", ");
+        str = str.substr(str, index);
+    }
     throw new DOMException(
-        "Cant find only one element with active scrollbar. Please create issue about it at https://github.com/Noksa/WebDriver.Screenshots.Extensions/issues");
+        `Cant find only one element with active scrollbar. Count of elements: ${elementWithScrollBar.length}. Elements tags: ${str}. Please create issue about it at https://github.com/Noksa/WebDriver.Screenshots.Extensions/issues`);
 }
 
 function FilteringElements(elements) {
     return $(elements).filter(function() {
         return $(this)[0].scrollHeight > $(this)[0].clientHeight &&
-            $(this)[0].scrollHeight > $(this).innerHeight() &&
-            ($(this).css("overflow").includes("auto") ||
-                $(this).css("overflow").includes("scroll") ||
-                $(this).css("overflow").includes("visible"));
+            $(this)[0].scrollHeight > $(this).innerHeight() && $(this)[0].scrollHeight > $(window).height() &&
+            !$(this).css("overflow") !== "hidden";
     });
 
 }
