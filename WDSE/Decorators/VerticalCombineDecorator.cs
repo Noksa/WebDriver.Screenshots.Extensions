97ce200bf8876bb9391b189c010381bc7d4883a5
Alexandr D <doom@ya.ru>
2018-10-29 08:18:05 +0300

    Improve VCS

--- WDSE/Decorators/VerticalCombineDecorator.cs
+++ WDSE/Decorators/VerticalCombineDecorator.cs
@@ -63,11 +63,10 @@ namespace WDSE.Decorators
             int totalHeight;
             var beforeActionsDocumentHeight = driver.GetHeight(SizesHelper.Entity.Document);
             var elementWithScrollBar = driver.GetElementWithActiveScrollBar();
-            if (elementWithScrollBar == null || elementWithScrollBar.TagName.ToLower() == "body" ||
+            if (elementWithScrollBar.TagName.ToLower() == "body" ||
                 elementWithScrollBar.TagName.ToLower() == "html")
             {
                 totalHeight = driver.GetHeight(SizesHelper.Entity.Document);
-                elementWithScrollBar = driver.GetDocumentScrollingElement();
             }
             else
             {
