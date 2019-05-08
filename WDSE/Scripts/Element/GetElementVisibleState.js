b29cb740871603f914352220a9a585b581b9389f
Noksa <a.danilin@iml.ru>
2019-05-08 09:05:25 +0300

    refactor to ie

--- WDSE/Scripts/Element/GetElementVisibleState.js
+++ WDSE/Scripts/Element/GetElementVisibleState.js
@@ -1,10 +1,10 @@
 ﻿function GetVisibleState(y, bottom) {
-    const win = $(window);
-    const elementTop = Math.max(y, 0);
-    const elementBottom = Math.min(bottom, win.height());
-    const viewportTop = 0;
-    const viewportBottom = win.height();
-    const result = elementTop >= viewportTop &&
+	var  win = $(window);
+	var  elementTop = Math.max(y, 0);
+	var  elementBottom = Math.min(bottom, win.height());
+	var  viewportTop = 0;
+	var  viewportBottom = win.height();
+	var  result = elementTop >= viewportTop &&
         elementTop < viewportBottom &&
         elementBottom <= viewportBottom &&
         elementBottom > viewportTop;
